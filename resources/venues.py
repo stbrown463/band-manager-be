2ef849d2474e7e5f77997f8abfca03d9b300a5b1
Sam Brown <stbrown463@gmail.com>
2019-03-07 18:36:08 -0600

    post and get all venues working

--- resources/venues.py
+++ resources/venues.py
@@ -5,7 +5,8 @@ from playhouse.shortcuts import model_to_dict, dict_to_model
 
 import models
 
-venur_fields = {
+venue_fields = {
+	'id': fields.Integer,
 	'name': fields.String,
 	'email': fields.String,
 	'img_url': fields.String,
@@ -18,15 +19,91 @@ venur_fields = {
 	'website': fields.String
 }
 
+class VenuesNew(Resource):
+	def __init__(self):
+	  self.reqparse = reqparse.RequestParser()
+	  self.reqparse.add_argument(
+	    'name',
+	    required=True,
+	    help='No band name provided',
+	    location=['form', 'json']
+	  )
+	  self.reqparse.add_argument(
+	    'img_url',
+	    required=False,
+	    help='No band name provided',
+	    location=['form', 'json']
+	  )
+	  self.reqparse.add_argument(
+	    'email',
+	    required=False,
+	    help='No band name provided',
+	    location=['form', 'json']
+	  )
+	  self.reqparse.add_argument(
+	    'streetAddress',
+	    required=False,
+	    help='No band name provided',
+	    location=['form', 'json']
+	  )
+	  self.reqparse.add_argument(
+	    'zipcode',
+	    required=False,
+	    help='No band name provided',
+	    location=['form', 'json']
+	  )
+	  self.reqparse.add_argument(
+	    'city',
+	    required=False,
+	    help='No band name provided',
+	    location=['form', 'json']
+	  )
+	  self.reqparse.add_argument(
+	    'country',
+	    required=False,
+	    help='No band name provided',
+	    location=['form', 'json']
+	  )
+	  self.reqparse.add_argument(
+	    'longitude',
+	    required=False,
+	    help='No band name provided',
+	    location=['form', 'json']
+	  )
+	  self.reqparse.add_argument(
+	    'latitude',
+	    required=False,
+	    help='No band name provided',
+	    location=['form', 'json']
+	  )
+	  self.reqparse.add_argument(
+	    'website',
+	    required=False,
+	    help='No band name provided',
+	    location=['form', 'json']
+	  )	  
+	  super().__init__()
+
+	## Create New Venue -- Working
+	@marshal_with(venue_fields) 
+	def post(self):
+		args = self.reqparse.parse_args()
+		print(args, 'hittingggg ')
+		venue = models.Venue.create(**args)
+		return venue
+
 class VenuesList(Resource):
 	def __init__(self):
 	  self.reqparse = reqparse.RequestParser()
 
-	## Get All Bands -- working
+	## Get All Venues === Untested
 	def get(self):
-
 		venues = [marshal(venue, venue_fields) for venue in models.Venue.select()]
-		return bands
+		return venues
+
+
+
+
 
 venues_api = Blueprint('resources.venues', __name__)
 api = Api(venues_api)
@@ -35,6 +112,12 @@ api.add_resource(
 	VenuesList,
 	'/venues',
 	endpoint="venues"
+	)
+
+api.add_resource(
+	VenuesNew,
+	'/venues/new',
+	endpoint="venues_new"
 	)
 
 
